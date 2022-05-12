6162338c20e8adb1701eca3309733b8b8442c293
solarjoni <solarjoni@gmail.com>
2022-05-12 18:18:46 +0300

    Unit, functional tests and routes implemented

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,33 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const Translator = require("../components/translator.js");
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    if (
+      !req.body.hasOwnProperty("text") ||
+      !req.body.hasOwnProperty("locale")
+    ) {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    if (req.body.text.length === 0) {
+      return res.json({ error: "No text to translate" });
+    }
+
+    if (
+      req.body.locale !== "american-to-british" &&
+      req.body.locale !== "british-to-american"
+    ) {
+      return res.json({ error: "Invalid value for locale field" });
+    }
+
+    const { text, locale } = req.body;
+
+    const translation = translator.translate(text, locale);
+
+    res.json({ text, translation });
+  });
 };
